9138e76f47b4c1a9cce8c228f60415d270d36e2f
Samirza0912 <samirza@code.edu.az>
2022-03-10 00:30:30 +0400

    first commit

---
+++ h-w/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace h_w
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
